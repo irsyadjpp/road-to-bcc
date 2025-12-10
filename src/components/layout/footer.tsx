75db6cfc37fc26e45c8cbf60afffcf1bc5faa28e
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2025-12-10 20:21:18 +0000

    hilangkan rekrutment panitia, volunteer, buku panduan teknis, FAQ

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,4 @@
+
 import { Mail, Phone, BookOpen, ArrowRight, HelpCircle, Users, HeartHandshake } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from '../ui/button';
@@ -23,30 +24,6 @@ export function Footer() {
           <div>
             <h3 className="font-headline text-lg font-bold mb-4 text-primary">Informasi</h3>
             <ul className="space-y-2 text-muted-foreground">
-              <li className="flex items-center justify-center md:justify-start">
-                <Users className="w-4 h-4 mr-2 text-primary/70" />
-                <Link href="/recruitment" className="hover:text-primary transition-colors">
-                  Rekrutmen Panitia
-                </Link>
-              </li>
-               <li className="flex items-center justify-center md:justify-start">
-                <HeartHandshake className="w-4 h-4 mr-2 text-primary/70" />
-                <Link href="/volunteer" className="hover:text-primary transition-colors">
-                  Jadi Volunteer
-                </Link>
-              </li>
-               <li className="flex items-center justify-center md:justify-start">
-                <BookOpen className="w-4 h-4 mr-2 text-primary/70" />
-                <a href="/technical-handbook.pdf" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">
-                  Buku Panduan Teknis
-                </a>
-              </li>
-              <li className="flex items-center justify-center md:justify-start">
-                <HelpCircle className="w-4 h-4 mr-2 text-primary/70" />
-                <Link href="/faq" className="hover:text-primary transition-colors">
-                  Pertanyaan Umum (FAQ)
-                </Link>
-              </li>
                  <li className="flex items-center justify-center md:justify-start">
                 <Mail className="w-4 h-4 mr-2 text-primary/70" />
                 <a href="mailto:admin@managementbcc.com" className="hover:text-primary transition-colors">
